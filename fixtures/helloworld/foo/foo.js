1c83549d82f6c85d4c3d38397f85fae04d64909e
Damian Beresford <Damian.Beresford@feedhenry.com>
2011-10-03 16:02:39 +0000

    Remove tmp zip file if it already exists

---
+++ fixtures/helloworld/foo/foo.js
@@ -0,0 +1,6 @@
+
+function hello() {
+  console.log('hello!');
+}
+
+exports.hello = hello;
--- fixtures/helloworld/main.js
+++ fixtures/helloworld/main.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var port = process.env.VCAP_APP_PORT || 8080;
 var util = require('util');
+var foo = require('./foo/foo.js');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log("Got request: " + util.inspect(req));
+  foo.hello();
   res.end('Hello World\n');
 }).listen(port, "127.0.0.1");
 console.log('Server running at on port: ' + port);
