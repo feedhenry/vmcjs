de4f4997e86b5c27b56225386573cf4511016d31
Brian McClain <brianmmcclain@gmail.com>
2012-03-24 01:46:35 -0400

    Added check when creating VMC class for either both username and password, or token

--- test/token_login.js
+++ test/token_login.js
@@ -6,17 +6,17 @@ var username = config.username;
 var password = config.password;
 
 // Get info using traditional login
-/*
+
 var vmc = new vmcjs.VMC("http://api.cloudfoundry.com", username, password);
 vmc.login(function(err) {
 	vmc.info(function(err, data){
-		console.dir(data);
+		console.dir(data.frameworks.node.runtimes[0].version);
 	});
 });
-*/
+
 
 // Get info using token login
-var vmc_token = new vmcjs.VMC("http://api.cloudfoundry.com", "username", "password", token);
+var vmc_token = new vmcjs.VMC("http://api.cloudfoundry.com", undefined, undefined, token);
 vmc_token.info(function(err, data){
-	console.dir(data);
+	console.dir(data.frameworks.node.runtimes[1].version);
 });
