a39f5d41668ea453c716d5fd8c4fd10c0ad62d29
Brian McClain <brianmmcclain@gmail.com>
2012-03-24 01:33:56 -0400

    Added token parameter to VMC class. If not passed, token is still set to undefined. If a token is passed, it's set to the session

--- test/token_login.js
+++ test/token_login.js
@@ -6,9 +6,17 @@ var username = config.username;
 var password = config.password;
 
 // Get info using traditional login
+/*
 var vmc = new vmcjs.VMC("http://api.cloudfoundry.com", username, password);
 vmc.login(function(err) {
 	vmc.info(function(err, data){
-		console.dir(data.frameworks.node.runtimes[0].version);
+		console.dir(data);
 	});
+});
+*/
+
+// Get info using token login
+var vmc_token = new vmcjs.VMC("http://api.cloudfoundry.com", "username", "password", token);
+vmc_token.info(function(err, data){
+	console.dir(data);
 });
